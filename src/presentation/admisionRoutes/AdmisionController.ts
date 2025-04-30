069cadc6578792078ac2bbe15ae81e8c4fc2ca8a
Máximo quiroga <123843504+MaxIIIDev@users.noreply.github.com>
2025-04-30 15:36:01 -0300

    Cambios en el admision Controller

--- src/presentation/admisionRoutes/AdmisionController.ts
+++ src/presentation/admisionRoutes/AdmisionController.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
+import { Conexion } from "../../data/conexion";
 
 
 export class AdmisionController{
-
+    private conexionBd: Conexion;
+    public constructor(conexionbd: Conexion){
+        this.conexionBd = conexionbd;
+    }
     public admitir = (req:Request,res:Response)=> {
-
+       
         res.render("prueba");
 
     }
--- src/presentation/admisionRoutes/AdmisionRoutes.ts
+++ src/presentation/admisionRoutes/AdmisionRoutes.ts
@@ -9,7 +9,7 @@ export class AdmisionRoutes{
     static admisionRoutes(conexionBd: Conexion): Router{
 
         const router = Router();
-        const controller = new AdmisionController();
+        const controller = new AdmisionController(conexionBd);
         router.get("/", controller.admitir );
 
 
