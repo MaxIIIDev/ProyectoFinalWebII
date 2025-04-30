c8283d53c67574e5d2304775e4e2b1e2053370c6
Máximo quiroga <123843504+MaxIIIDev@users.noreply.github.com>
2025-04-30 15:45:17 -0300

    otra mas

--- src/presentation/Enfermeria/EnfermerosController.ts
+++ src/presentation/Enfermeria/EnfermerosController.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 
 export class EnfermerosController{
 
-    public getEnfermeros(req:Request,res:Response):void{
+    public getEnfermeros = (req:Request,res:Response):void => {
         res.send("hola estas en el metodo get enfermeros");
     }
 
