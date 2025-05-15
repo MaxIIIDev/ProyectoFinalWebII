692f319625c95a16111acb232ceb82be97d44dae
Máximo quiroga <123843504+MaxIIIDev@users.noreply.github.com>
2025-05-15 17:22:02 -0300

    tabla tipo de medicamento con su relacion

--- src/data/models/medicamentos.ts
+++ src/data/models/medicamentos.ts
@@ -1,6 +1,7 @@
-import { AllowNull, AutoIncrement, Column, DataType, HasMany, HasOne, Length, Model, NotNull, PrimaryKey, Table, Unique } from "sequelize-typescript";
+import { AllowNull, AutoIncrement, Column, DataType, ForeignKey, HasMany, HasOne, Length, Model, NotNull, PrimaryKey, Table, Unique } from "sequelize-typescript";
 import { Paciente_recetas } from "./paciente_recetas";
 import { paciente_tratamientos } from "./paciente_tratamientos";
+import { Tipo_De_Medicamento } from "./tipo_de_medicamento";
 
 @Table
 export class Medicamentos extends Model{
@@ -16,12 +17,12 @@ export class Medicamentos extends Model{
     @Column(DataType.STRING)
     declare nombre:string;
 
-    @AllowNull(false)
-    @NotNull
-    @Length({min:3,max:100})
-    @Column(DataType.STRING)
-    declare tipo:string;
-
+    @ForeignKey(()=> Tipo_De_Medicamento)
+    @Column(DataType.INTEGER)
+    declare id_tipo_de_medicamento:number;
+    
+    @HasOne(()=> Tipo_De_Medicamento)
+    declare tipo_de_medicamento: Tipo_De_Medicamento
     @AllowNull(false)
     @NotNull
     @Column(DataType.DOUBLE)
---
+++ src/data/models/tipo_de_medicamento.ts
@@ -0,0 +1,19 @@
+import { AutoIncrement, BelongsTo, Column, DataType, Model, PrimaryKey, Table } from "sequelize-typescript";
+import { Medicamentos } from "./medicamentos";
+
+
+@Table
+export class Tipo_De_Medicamento extends Model{
+
+    @PrimaryKey
+    @AutoIncrement
+    @Column(DataType.INTEGER)
+    declare id_tipo_de_medicamento: number
+
+    @Column(DataType.STRING)
+    declare nombre: string
+
+    @BelongsTo(()=> Medicamentos)
+    declare medicamentos: Medicamentos
+
+}
